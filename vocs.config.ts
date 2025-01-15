66627f7e9b9fbf3118143c7f1f6984e159bbb528
TurboFolkBlackMetal <mislav.javor@biconomy.io>
2025-01-15 13:50:57 +0100

    update

--- vocs.config.ts
+++ vocs.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "vocs";
 
 export default defineConfig({
   iconUrl: {
-    dark: "https://docs.biconomy.io/logo.svg",
-    light: "https://docs.biconomy.io/logo.svg",
+    dark: "https://i.imgur.com/WyX6ebY.png",
+    light: "https://i.imgur.com/WyX6ebY.png",
   },
   logoUrl: {
     dark: "https://docs.biconomy.io/logo.svg",
@@ -12,15 +12,26 @@ export default defineConfig({
   theme: {
     accentColor: "#ec5d31",
   },
+  socials: [
+    { 
+      icon: 'x',
+      link: 'https://x.com/biconomy'
+    }
+  ],
   sidebar: [
     {
       text: "Why Choose Biconomy?",
       link: "/about",
     },
+    { text: "AbstractJS SDK", link: "/abstractjs" },
+    { text: "Demo Videos", link: ''},
     {
       text: "Biconomy Stack Explained",
       items: [
-        { text: "AbstractJS", link: "/abstractjs" },
+        {
+          text: "Modular Smart Accounts",
+          link: "/explained/modular-sca",
+        },
         {
           text: "Modular Execution Environment",
           link: "/explained/mee",
@@ -29,15 +40,12 @@ export default defineConfig({
           text: "Bundlers and Paymasters",
           link: "/explained/bundlers-paymasters",
         },
-        {
-          text: "Nexus Account & Modules",
-          link: "/explained/nexus",
-        },
         { text: "EOA Support: Fusion and EIP7702", link: "/explained/eoa" },
       ],
     },
     {
-      text: "Bundlers & Paymasters",
+      text: "Using Bundlers & Paymasters",
+      collapsed: true,
       items: [
         { text: "Quickstart Guide", link: "/legacy/quickstart" },
         { text: "How to Sponsor Gas", link: "/legacy/tutorials/gasless" },
@@ -52,17 +60,46 @@ export default defineConfig({
       ],
     },
     {
-      text: "Modular Execution Environment",
+      text: "Using Modular Execution",
+      collapsed: true,
       items: [
-        { text: "Quickstart Guide", link: "/mee/quickstart" },
+        { text: "Overview", link: "/mee/mee" },
+        { text: ""},
+        { text: "→ Quickstart: Chain Abstracted App",
+           link: "/mee/quickstart" },
         {
-          text: "Multichain Smart Accounts",
-          link: "/mee/multichain-smart-account",
+          text: "→ Execute a Simple Supertransaction",
+          link: "/mee/tutorials/execute-supertransaction",
         },
-        { text: "Multichain Contracts", link: "/mee/multichain-contract" },
-        { text: "Bridging Plugins", link: "/mee/utils/bridgingplugin" },
         {
-          text: "Utilities",
+          text: "→ Execute an Intent",
+          link: "/mee/tutorials/execute-intent",
+        },
+        
+        {
+          text: "→ Execute a Cross-Chain Sequence",
+          link: "/mee/tutorials/combine-intents-transactions",
+        },
+        {
+          text: '→ Use EOAs Like Smart Accounts',
+          link: '/fusion/fusion'
+        },
+        {
+          text: ""
+        },
+        {
+          text: 'Concepts',
+          collapsed: true,
+          items: [
+            { text: 'Multichain Smart Account' },
+            { text: 'Supertransaction' },
+            { text: 'MultichainContract' },
+            { text: 'BridgePlugin' },
+          ]
+        },
+        {
+          text: "SDK Reference",
+          collapsed: true,
           items: [
             {
               text: "getUnifiedERC20Balance",
@@ -77,25 +114,27 @@ export default defineConfig({
       ],
     },
     {
+      text: "Account Modules",
+      collapsed: true,
+      items: [
+        { text: "Overview" },
+        { text: "Passkeys" },
+        { text: "Smart Sessions" },
+        { text: "Account Recovery" },
+      ],
+    },
+    {
       text: "EIP7702",
       items: [
         { text: "Wallet Integration Guide", link: "/eip7702/wallet-guide" },
       ],
     },
     {
-      text: "Account Modules",
+      text: "Resources",
       items: [
-        { text: "About Account Modules" },
-        { text: "Passkeys" },
-        { text: "Smart Sessions" },
+        { text: "Contracts & Audits", link: "/legacy/contractsAndAudits" },
+        { text: "Supported Networks", link: "/legacy/supportedNetworks" },
       ],
-    },
-    {
-      text: 'Resources',
-      items: [
-        { text: 'Contracts & Audits', link: '/legacy/contractsAndAudits' },
-        { text: 'Supported Networks', link: '/legacy/supportedNetworks' },
-      ]
     },
     {
       text: "Feature comparison - MEE vs AA",
